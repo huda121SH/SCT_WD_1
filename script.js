e983efb78716b4fb75afa26820f1d49c9d15d80d
huda121SH <hudahs1817@gmail.com>
2024-11-08 22:23:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('.nav-menu a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+window.addEventListener('scroll', function () {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+
+function toggleMenu() {
+    const navMenu = document.getElementById("nav-menu");
+        navMenu.classList.toggle("show");
+}
+
+
